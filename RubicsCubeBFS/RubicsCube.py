7c272da3e706b2385845cd2d6a141094f7a5688f
fulopbence <fulop.bence@sztaki.hu>
2024-05-03 17:33:56 +0200

    [RubicsCube] Rethinked structure of the cubelets

--- RubicsCubeBFS/RubicsCube.py
+++ RubicsCubeBFS/RubicsCube.py
@@ -1,21 +1,27 @@
 # RubicsCube
 # Author: Bence Fulop
 
-class RubicsCube:
-    def __init__(self):
-        self.front = ['ULW', 'URW', 'DLW', 'DRW']  # Up Left White, Up Right White, Down Left White, Down Right White
-        self.back = ['ULG', 'URG', 'DLG', 'DRG']   # Up Left Green, Up Right Green, Down Left Green, Down Right Green
-        self.left = ['ULY', 'URY', 'DLY', 'DRY']   # Up Left Yellow, Up Right Yellow, Down Left Yellow, Down Right Yellow
-        self.right = ['ULO', 'URO', 'DLO', 'DRO']  # Up Left Orange, Up Right Orange, Down Left Orange, Down Right Orange
-        self.up = ['ULP', 'URP', 'DLP', 'DRP']     # Up Left Purple, Up Right Purple, Down Left Purple, Down Right Purple
-        self.down = ['ULR', 'URR', 'DLR', 'DRR']   # Up Left Red, Up Right Red, Down Left Red, Down Right Red
-
-
-if __name__ == "__main__":
-    cube = RubicsCube()
-    print("Front:", cube.front)
-    print("Back:", cube.back)
-    print("Left:", cube.left)
-    print("Right:", cube.right)
-    print("Up:", cube.up)
-    print("Down:", cube.down)
+rgw = flu = 0
+gwr = luf = 1
+wrg = ufl = 2
+rwb = fur = 3
+wbr = urf = 4
+brw = rfu = 5
+ryg = fdl = 6
+ygr = dlf = 7
+gry = lfd = 8
+rby = frd = 9
+byr = rdf = 10
+yrb = dfr = 11
+owg = bul = 12
+wgo = ulb = 13
+gow = lbu = 14
+obw = bru = 15
+bwo = rub = 16
+wob = ubr = 17
+ogy = bld = 18
+gyo = ldb = 19
+yog = dbl = 20
+oyb = bdr = 21
+ybo = drb = 22
+boy = rbd = 23
